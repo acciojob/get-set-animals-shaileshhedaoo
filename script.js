6b6ce9e8c249a2a688eee7d1e10ff85342a29a6d
shaileshhedaoo <100752372+shaileshhedaoo@users.noreply.github.com>
2024-08-19 11:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound (this can be overridden by subclasses)
+  makeSound() {
+    console.log(`${this._species} makes a sound.`);
+  }
+}
+
+class Dog extends Animal {
+  // Method to make the dog bark
+  bark() {
+    console.log('woof');
+  }
+}
+
+class Cat extends Animal {
+  // Method to make the cat purr
+  purr() {
+    console.log('purr');
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
